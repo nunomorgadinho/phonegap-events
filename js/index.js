9f435b63f44248e5b17020c812e9795af2f9335d
Nuno Morgadinho <nuno+rulez@widgilabs.com>
2014-10-17 00:44:20 +0100

    testing events

--- js/index.js
+++ js/index.js
@@ -26,6 +26,11 @@ var app = {
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
+        
+        $(app.onDOMLoad());
+    },
+    onDOMLoad: function() {
+        $('#appdata').append("DOM Loaded <br><br>");
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
@@ -33,19 +38,9 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+         $('#appdata').append("Device Ready <br><br>");
 
-        $('#appdata').append("Received Event: " + id);
-
-        console.log('Received Event: ' + id);
     }
+
 };
