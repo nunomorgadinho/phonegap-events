e4b52285f21906c93b501e4a127ec5bfadaf18e5
Nuno Morgadinho <nuno+rulez@widgilabs.com>
2014-10-17 01:01:49 +0100

    testing events

--- js/index.js
+++ js/index.js
@@ -64,7 +64,7 @@ var app = {
     countPause: function() {
         count++;
     },
-    onMenuBtnDown: function() {
+    onMenuBtnDown: function(e) {
          e.preventDefault();
          $('#appdata').append("Menu Button Pressed <br><br>");
     },
